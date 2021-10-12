5643526ac410599c00da49c1af897855b31708a3
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-10-12 13:53:57 +0900

    add Views for MainViewController

---
+++ eonggeum/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  eonggeum
+//
+//  Created by YOONJONG on 2021/10/11.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var todayView: UIView!
+    @IBOutlet weak var todayCountView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+
+    func updateUI(){
+        todayView.layer.cornerRadius = 20
+        todayCountView.layer.cornerRadius = 20
+//        todayCountView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        
+    }
+
+}
