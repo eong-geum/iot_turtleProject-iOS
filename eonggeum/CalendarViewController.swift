dc312c4b23343323588a6811c82e92eff478853b
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-10-13 00:34:27 +0900

    Change subtitleFont, offset of Calendar

--- eonggeum/CalendarViewController.swift
+++ eonggeum/CalendarViewController.swift
@@ -23,12 +23,14 @@ class CalendarViewController: UIViewController {
     
     func UIUpdate(){
         calendarView.appearance.todayColor = UIColor(red: 7/255, green: 181/255, blue: 129/255, alpha: 1)
-        calendarView.allowsSelection = false
+        calendarView.allowsSelection = true
         calendarView.appearance.headerTitleColor = .black
         calendarView.appearance.weekdayTextColor = .black
         calendarView.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24, weight: .bold)
         calendarView.appearance.titleWeekendColor = UIColor(red: 7/255, green: 181/255, blue: 129/255, alpha: 1)
         calendarView.appearance.headerDateFormat = "YYYY년 M월"
+        calendarView.appearance.subtitleFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+        calendarView.appearance.subtitleOffset = CGPoint(x: 0, y: 3)
         calendarView.locale = Locale(identifier: "ko_KR")
     }
     
@@ -38,13 +40,15 @@ extension CalendarViewController :FSCalendarDelegate, FSCalendarDataSource, FSCa
     func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
         switch dateFormatter.string(from: date){
         case dateFormatter.string(from: Date()):
-            return "4"
+            return "4회"
+        case "2021-10-12":
+            return "17회"
         case "2021-10-11":
-            return "17"
+            return "13회"
         case "2021-10-10":
-            return "15"
+            return "15회"
         case "2021-10-09":
-            return "18"
+            return "18회"
         default:
             return nil
         }
