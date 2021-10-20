bda5e266038991d6772b81f71dd839862e974769
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-10-20 21:32:03 +0900

    Add SRCountdownTimer delegate

--- eonggeum/TimerViewController.swift
+++ eonggeum/TimerViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import SRCountdownTimer
 
-class TimerViewController: UIViewController {
+class TimerViewController: UIViewController, SRCountdownTimerDelegate {
 
     @IBOutlet weak var countdownTimer: SRCountdownTimer!
     @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
+        countdownTimer.delegate = self
         super.viewDidLoad()
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
@@ -28,12 +29,16 @@ class TimerViewController: UIViewController {
         countdownTimer.trailLineColor = UIColor.lightGray.withAlphaComponent(0.5)
         countdownTimer.isLabelHidden = false
         countdownTimer.labelFont = UIFont.systemFont(ofSize: 80, weight: .light)
+        countdownTimer.timerFinishingText = "완료!"
     }
     private func updateButtonUI(){
         startButton.layer.cornerRadius = 30
         startButton.layer.backgroundColor = CGColor(red: 22/255, green: 217/255, blue: 129/255, alpha: 1)
     }
     @IBAction func startButtonTapped(_ sender: Any) {
-        countdownTimer.start(beginingValue: 30, interval: 1)
+        countdownTimer.start(beginingValue: 10, interval: 1)
+    }
+    func timerDidEnd(sender: SRCountdownTimer, elapsedTime: TimeInterval) {
+        
     }
 }
